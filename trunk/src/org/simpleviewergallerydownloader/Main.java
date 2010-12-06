7c3103c932e071935f8cce46187c6f5bfb06bfdb
ivan.a.galkin <ivan.a.galkin@14b409b8-dc90-eb28-d486-6553a42d9bc6>
2010-12-06 12:25:33 +0000

    Initial project import

---
+++ trunk/src/org/simpleviewergallerydownloader/Main.java
@@ -0,0 +1,50 @@
+package org.simpleviewergallerydownloader;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+/**
+ * Start {@link Downloader} with {@link SwingUI}
+ * 
+ * @author ivangalkin
+ * 
+ */
+public class Main {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main window = new Main();
+					window.frame.setContentPane(new SwingUI());
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Main() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame("Downloader for SimpleviewerGallery");
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
