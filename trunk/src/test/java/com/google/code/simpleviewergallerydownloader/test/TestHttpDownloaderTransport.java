438cc410d9468e1ab194d83d89f850cb8c0547db
ivan.a.galkin <ivan.a.galkin@14b409b8-dc90-eb28-d486-6553a42d9bc6>
2012-06-24 19:22:28 +0000

    tests, svn settings and maven assebembly settings

--- trunk/src/test/java/com/google/code/simpleviewergallerydownloader/test/TestHttpDownloaderTransport.java
+++ trunk/src/test/java/com/google/code/simpleviewergallerydownloader/test/TestHttpDownloaderTransport.java
@@ -1,5 +1,6 @@
 package com.google.code.simpleviewergallerydownloader.test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -7,6 +8,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.commons.io.IOUtils;
 import org.junit.Test;
@@ -18,7 +21,8 @@ import com.google.code.simpleviewergallerydownloader.IDownloaderTransport.Downlo
 public class TestHttpDownloaderTransport {
 
 	@Test
-	public void testDownload() throws DownloaderTransportException, IOException {
+	public void testDownloadOneFile() throws DownloaderTransportException,
+			IOException {
 		IDownloaderTransport httpTransport = new HttpDownloaderTransport(
 				"test", "test");
 		InputStream file = httpTransport.download(new URL(
@@ -30,6 +34,41 @@ public class TestHttpDownloaderTransport {
 		IOUtils.copy(file, content);
 
 		assertTrue(content.size() > 0);
+	}
+
+	@Test
+	public void testDownloadFileMultipleTimes()
+			throws DownloaderTransportException, IOException {
+		int T = 10;
+
+		IDownloaderTransport httpTransport = new HttpDownloaderTransport(
+				"test", "test");
+		List<ByteArrayOutputStream> contentList = new ArrayList<ByteArrayOutputStream>(
+				T);
+
+		for (int i = 0; i < T; i++) {
+			InputStream file = httpTransport.download(new URL(
+					"http://browserspy.dk/password-ok.php"));
+
+			assertNotNull(file);
+
+			ByteArrayOutputStream content = new ByteArrayOutputStream();
+			IOUtils.copy(file, content);
+			assertTrue(content.size() > 0);
+			contentList.add(content);
+		}
+
+		for (int i = 1; i < T; i++) {
+			assertEquals(contentList.get(0).size(), contentList.get(i).size());
+		}
+	}
+
+	@Test
+	public void testDownloadFileMultipleTimesMultipleInstances()
+			throws DownloaderTransportException, IOException {
+		for (int i = 0; i < 10; i++) {
+			testDownloadFileMultipleTimes();
+		}
 	}
 
 }
